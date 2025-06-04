ad4a1f21ff5ef0d35c5690d6568ca2cc749853f6
文婷 <2103674651@qq.com>
2025-06-04 11:43:07 +0800

    Update 4.c

--- SF_week3/Project1/Project1/4.c
+++ SF_week3/Project1/Project1/4.c
@@ -1,34 +1,30 @@
+//设计两个状态：
+//dp[n][0]: 长度为 n 的排列，首尾不相同
+//dp[n][1]: 长度为 n 的排列，首尾相同
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 #define MAX_N 50
 
-// �洢Ԥ�������������
-long long ans[MAX_N + 1];
+long long dp[MAX_N + 1][2];
 
-// Ԥ�������п��ܵĽ��
 void preprocess() {
-    ans[1] = 3;  // �� n = 1 ʱ��ֱ�ӷ��� 3
-    long long prev0 = 0, prev1 = 3;  // �ֱ��Ӧ dp[1][0] �� dp[1][1]
+    dp[1][0] = 3;  // 首尾不同
+    dp[1][1] = 0;  // 首尾相同不可能
 
-    for (int k = 2; k <= MAX_N; k++) {
-        long long curr0 = prev0 * 1 + prev1 * 2;
-        long long curr1 = prev0 * 1;
-        ans[k] = curr0;
-        prev0 = curr0;
-        prev1 = curr1;
+    for (int i = 2; i <= MAX_N; ++i) {
+        dp[i][0] = dp[i - 1][0] * 1 + dp[i - 1][1] * 2; //首尾不同
+        dp[i][1] = dp[i - 1][0] * 1; //首尾相同
     }
 }
 
 int main() {
     int n;
-
-    // Ԥ�������
     preprocess();
 
-    // ��ȡ���벢������
     while (scanf("%d", &n) == 1) {
-        printf("%lld\n", ans[n]);
+        printf("%lld\n", dp[n][0]);
     }
 
     return 0;
